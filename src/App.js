bf7ee59709d676eb7e3b94ca1d3e19a1d36efc31
Jen <jen.lijo@zalando.de>
2018-07-10 11:48:32 +0200

    Chart data draft

--- src/App.js
+++ src/App.js
@@ -2,6 +2,47 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var BarChart = require("react-chartjs").Bar;
+
+
+var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+var chartData = {
+	labels: ["January", "February", "March", "April", "May", "June", "July"],
+	datasets: [
+		{
+			label: "My First dataset",
+			fillColor: "rgba(220,220,220,0.5)",
+			strokeColor: "rgba(220,220,220,0.8)",
+			highlightFill: "rgba(220,220,220,0.75)",
+			highlightStroke: "rgba(220,220,220,1)",
+			data: [65, 59, 80, 81, 56, 55, 40]
+		},
+		{
+			label: "My Second dataset",
+			fillColor: "rgba(151,187,205,0.5)",
+			strokeColor: "rgba(151,187,205,0.8)",
+			highlightFill: "rgba(151,187,205,0.75)",
+			highlightStroke: "rgba(151,187,205,1)",
+			data: [28, 48, 40, 19, 86, 27, 90]
+		}
+	]
+};
+
+// const chartOptions = ["One", "Two"];
+// const chartData = [1, 2,4,6,7];
+
+var chartOptions = {
+  responsive: true,
+  legend: {
+    position: 'top',
+  },
+  title: {
+    display: true,
+    text: 'Chart.js Bar Chart'
+  }
+};
+
 class App extends Component {
   render() {
     return (
@@ -11,7 +52,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <BarChart data={chartData}  width="600" height="250"/>
         </p>
       </div>
     );
