d4be737dcdbc3a49a805f1829b1398c1e990770d
Jen <jen.lijo@zalando.de>
2018-07-10 12:15:56 +0200

    BarChart - Clear data

--- src/App.js
+++ src/App.js
@@ -37,23 +37,16 @@ class App extends Component {
     this.barData = {
       labels: ["Before Course", "After Course"],
       datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        label: '% Knowledge',
+        data: [19, 19],
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
+          'rgba(54, 162, 235, 0.2)'
         ],
         borderColor: [
           'rgba(255,99,132,1)',
           'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
+          'rgba(255, 206, 86, 1)'
         ],
         borderWidth: 1
       }]
@@ -69,7 +62,6 @@ class App extends Component {
         </header>
         <p className="App-intro">
           <Doughnut data={this.chartData} />
-          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
 
         <br />
