b127283e089e0b72b6212d6dee5dc12e17c78b29
Jen <jen.lijo@zalando.de>
2018-07-10 14:46:59 +0200

    Tweaks and that

--- src/App.js
+++ src/App.js
@@ -1,139 +1,136 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-import {Doughnut} from 'react-chartjs-2';
-import {Bar} from 'react-chartjs-2';
+import { Doughnut } from "react-chartjs-2";
+import { Bar } from "react-chartjs-2";
 
 class App extends Component {
-
   constructor(args) {
     super(args);
-    this.chartData = {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-      }]
-    };
-
     this.barData = {
       labels: ["Before Course", "After Course"],
-      datasets: [{
-        label: '% Knowledge',
-        data: [40, 90],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)'
-        ],
-        borderWidth: 1
-      }]
-    }
+      datasets: [
+        {
+          label: "% Knowledge",
+          data: [40, 90],
+          backgroundColor: [
+            "rgba(211, 216, 0)",
+            "rgba(116, 192, 153)" // Mint
+          ],
+          borderWidth: 0
+        }
+      ]
+    };
 
-    this.doughnutRight = {
+    this.doughtnutL = {
       labels: ["Workers", "Supervisors", "Managers"],
-      datasets: [{
-        label: '% Workers',
-        data: [40, 25, 10],
-        backgroundColor: [
-          'rgba(116, 192, 153)', // Mint
-          'rgba(255, 102, 102)',
-          'rgba(0, 60, 65)' // dark blue
-        ],
-        borderColor: [
-          'rgba(116, 192, 153)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(0, 60, 65)'
-        ],
-        borderWidth: 0
-      }]
-    }
+      datasets: [
+        {
+          label: "% Workers",
+          data: [40, 25, 10],
+          backgroundColor: [
+            "rgba(116, 192, 153)", // Mint
+            "rgba(255, 102, 102)",
+            "rgba(0, 60, 65)" // dark blue
+          ],
+          borderColor: [
+            "rgba(116, 192, 153)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(0, 60, 65)"
+          ],
+          borderWidth: 0
+        }
+      ]
+    };
 
-    this.doughnutLeft = {
+    this.doughnutR = {
       labels: ["Enrolled in this course", "Not enrolled"],
-      datasets: [{
-        label: '% Enrolled',
-        data: [60, 40],
-        backgroundColor: [
-          'rgba(116, 192, 153)', // Mint
-          'rgba(0, 60, 65)' // dark blue
-        ],
-        borderColor: [
-          'rgba(116, 192, 153)', // Mint
-          'rgba(0, 60, 65)' // dark blue
-        ],
-        borderWidth: 1
-      }]
-    }
+      datasets: [
+        {
+          label: "% Enrolled",
+          data: [60, 40],
+          backgroundColor: [
+            "rgba(116, 192, 153)", // Mint
+            "rgba(0, 60, 65)" // dark blue
+          ],
+          borderColor: [
+            "rgba(116, 192, 153)", // Mint
+            "rgba(0, 60, 65)" // dark blue
+          ],
+          borderWidth: 1
+        }
+      ]
+    };
   }
 
   render() {
     return (
       <div className="App">
-        <header>
-          <p>View all cards</p>
-        </header>
+        <header />
 
-        <p><strong>
-          Result: Knowledge before and after course
-        </strong></p>
-        <p>
-        <Bar
-          data={this.barData}
-          width={500}
-          height={500}
-          options={{
-            maintainAspectRatio: false,
-            scales: {
-              "yAxes": [{"ticks": { "beginAtZero": true, "max": 100}}]
-            }
-          }}
-        />
-        </p>
-
-        <br />
-        <br />
-        <br />
         <div className="wrapper">
-          <p className="doughnut-1">
-          <p><strong>User demographics: Role</strong></p>
-            <Doughnut
-              data={this.doughnutRight}
+          <div className="bar">
+            <p>
+              <strong>Result: Knowledge before and after course</strong>
+            </p>
+            <Bar
+              data={this.barData}
               width={500}
               height={500}
+              options={{
+                maintainAspectRatio: false,
+                scales: {
+                  yAxes: [{ ticks: { beginAtZero: true, max: 100 } }]
+                }
+              }}
             />
-          </p>
+          </div>
 
-          <p className="doughnut-2">
-          <p><strong>Employees enrolled vs not enrolled</strong></p>
+          <br />
+          <br />
+          <br />
+          <div className="doughnut-1">
+            <p>
+              <strong>User demographics: Role</strong>
+            </p>
             <Doughnut
-              data={this.doughnutLeft} 
+              data={this.doughtnutL}
               width={500}
               height={500}
+              options={{
+                legend: {
+                  position: "bottom",
+                  fullWidth: true
+                }
+              }}
             />
-          </p>
+            {/*
+              <p> Workers: {this.doughtnutL.datasets[0].data[0]} </p>
+              <p> Supervirsors: {this.doughtnutL.datasets[0].data[1]} </p>
+              <p> Managers: {this.doughtnutL.datasets[0].data[2]} </p>
+            */}
+          </div>
+
+          <div className="doughnut-2">
+            <p><strong>Employees enrolled vs not enrolled</strong></p>
+            <Doughnut
+              data={this.doughnutR}
+              width={500}
+              height={500}
+              options={{
+                legend: {
+                  position: "bottom",
+                  fullWidth: true
+                }
+              }}
+            />
+            
+            {/*
+              <p>Enrolled in this course: {this.doughnutR.datasets[0].data[0]}</p>
+              <p> Not enrolled: {this.doughnutR.datasets[0].data[1]} </p>
+            */}
+          </div>
         </div>
       </div>
     );
