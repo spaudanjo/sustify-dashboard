8057958e132ac29909d8526d2703e6c774067d5d
Daniel Spaude <daniel@spaude.de>
2018-07-10 11:54:13 +0200

    add chart-js with simple chart

--- src/App.js
+++ src/App.js
@@ -2,7 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {Doughnut} from 'react-chartjs-2';
+
 class App extends Component {
+
+  constructor(args) {
+    super(args);
+    this.chartData = {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,6 +42,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
+          <Doughnut data={this.chartData} />
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
