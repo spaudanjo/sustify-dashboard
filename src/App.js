87d74dace496b2a1e632779809775fcf432dcaa5
Jen <jen.lijo@zalando.de>
2018-07-10 12:08:21 +0200

    Add different data for bar chart

--- src/App.js
+++ src/App.js
@@ -33,6 +33,31 @@ class App extends Component {
         borderWidth: 1
       }]
     };
+
+    this.barData = {
+      labels: ["Before Course", "After Course"],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    }
   }
 
   render() {
@@ -47,11 +72,17 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
 
+        <br />
+        <br />
+        <br />
+        <p><strong>
+          Result: Knowledge before and after course
+        </strong></p>
         <p>
         <Bar
-          data={this.chartData}
-          width={100}
-          height={50}
+          data={this.barData}
+          width={500}
+          height={500}
           options={{
             maintainAspectRatio: false
           }}
